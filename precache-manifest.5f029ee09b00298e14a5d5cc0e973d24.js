1ad7ed6e65b4c35f0143421cac6b3fd17b85033a
Nicolas Besson <billounet@gmail.com>
2019-08-07 15:16:28 +0200

    Updates

---
+++ precache-manifest.5f029ee09b00298e14a5d5cc0e973d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93d99ca2e745e23f96154840f83718c",
+    "url": "/meistertask-report/index.html"
+  },
+  {
+    "revision": "661a2c7cc848ab5015b2",
+    "url": "/meistertask-report/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "5e687ca1c088494e45b4",
+    "url": "/meistertask-report/static/css/main.6bb7f1fc.chunk.css"
+  },
+  {
+    "revision": "661a2c7cc848ab5015b2",
+    "url": "/meistertask-report/static/js/2.e62889fc.chunk.js"
+  },
+  {
+    "revision": "5e687ca1c088494e45b4",
+    "url": "/meistertask-report/static/js/main.3f19a320.chunk.js"
+  },
+  {
+    "revision": "b47101ae2ff67ac78fa5",
+    "url": "/meistertask-report/static/js/runtime~main.8b33ec37.js"
+  }
+]);
