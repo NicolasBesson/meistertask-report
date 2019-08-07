7d53249e81ff2856eb5536e10df9130686bfc9b0
Nicolas Besson <billounet@gmail.com>
2019-08-07 08:31:56 +0200

    Shiny configuration page

--- meisterreport/src/config/AppConfig.js
+++ meisterreport/src/config/AppConfig.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { AppContext } from '../context/Context';
+import "./AppConfig.css";
 
 class AppConfig extends React.Component {
 
@@ -38,19 +39,25 @@ class AppConfig extends React.Component {
             <div>
                 <AppContext.Consumer>
                     {({token, projectID}) => (
-                    <form>
-                        <div>
-                            <label>Jeton de sécurité</label>
-                            <input id="tokenValue" name="securityToken" defaultValue={token} required onChange={this.handleTokenChange}/>
+                        <div className="wrapper fadeInDown">
+                        <div id="formContent">
+                            
+                            <div className="fadeIn first">
+                            <img src="./assets/settings.svg" id="icon" alt="User Icon" height="60" width="60" />
+                            </div>
+
+                            <form>
+                            <input type="text" id="tokenValue" className="fadeIn second" name="securityToken" placeholder="Jeton de sécurité" required onChange={this.handleTokenChange}/>
+                            <input type="text" id="poject" className="fadeIn third" name="projectID" placeholder="Numéro de projet" required onChange={this.handleProjectChange}/>
+                            <input type="submit" className="fadeIn fourth" value="Valider" onClick={this.handleSubmitClick}/>
+                            </form>
+
+                            <div id="formFooter">
+                            <a className="underlineHover" href="https://github.com/NicolasBesson/meistertask-report/blob/master/doc/configuration.md">Paramètres oubliés?</a>
+                            </div>
+
                         </div>
-                        <div>
-                            <label>Numéro de projet</label>
-                            <input id="poject" name="projectID" defaultValue={projectID} required onChange={this.handleProjectChange}/>
                         </div>
-                        <div>
-                            <button onClick={this.handleSubmitClick}>Valider</button>
-                        </div>
-                    </form>
                     )}
                 </AppContext.Consumer>
             </div>
